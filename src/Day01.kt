5ae0061f0a2e040fa371d4a4f6bfe6c241607281
William <william@changedapps.com>
2023-12-01 12:33:01 -0600

    Finished part 1 of day01 challenge.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sumOfAllDigits = 0
+        input.forEach { item ->
+            val filterAllDigits = item.filter { it.isDigit() }
+            val first = filterAllDigits.first().toString()
+            val second = filterAllDigits.last().toString()
+            val result = (first+second).toInt()
+            sumOfAllDigits += result
+        }
+        return sumOfAllDigits
     }
+
+
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val testInput = readInput("inputFiles/Day01_test")
+    val input = readInput("inputFiles/Day01")
+    println("This is Test Data:")
+    part1(testInput).println()
+    println("This is the actual input: ")
     part1(input).println()
-    part2(input).println()
 }
