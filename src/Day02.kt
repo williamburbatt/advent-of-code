7b167239e3f3e9ff63326174a18bf45be877ca61
William <william@changedapps.com>
2023-12-02 08:05:44 -0600

    Finished Day 2 challenge.

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,50 +1,35 @@
+val possibleIDList = ArrayList<Int>()
+val allPowers = ArrayList<Int>()
+
 fun main() {
     fun part1(input: List<String>): Int {
-        val MAX_RED = 12
-        val MAX_GREEN = 13
-        val MAX_BLUE = 14
-        val possibleIDList = ArrayList<Int>()
+
         input.forEach { singleInputString ->
             val inputSplit = singleInputString.split(":")
             val gameNumber = inputSplit[0].replace("Game ","").toInt()
             possibleIDList.add(gameNumber)
-            println("Game: $gameNumber")
+
             val fullGame = inputSplit[1].trim()
             val games = fullGame.split(";")
-            for(game in games){
-                val gameSplit = game.trim().split(",")
-                gameSplit.println()
-                gameSplit.forEach { singleCube ->
-                    when {
-                        singleCube.contains("red") -> {
-                            val redNum = singleCube.replace(" red","").trim()
-                            if (redNum.toInt() > MAX_RED){
-                                possibleIDList.remove(gameNumber)
-                            }
-                        }
-                        singleCube.contains("green") -> {
-                            val greenNum = singleCube.replace(" green","").trim()
-                            if (greenNum.toInt() > MAX_GREEN){
-                                possibleIDList.remove(gameNumber)
-                            }
-                        }
-                        singleCube.contains("blue") -> {
-                            val blueNum = singleCube.replace(" blue","").trim()
-                            if (blueNum.toInt() > MAX_BLUE){
-                                possibleIDList.remove(gameNumber)
-                            }
-                        }
-                    }
-                }
-            }
 
+            handleInvalidGames(games, gameNumber)
         }
         possibleIDList.println()
         return possibleIDList.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        input.forEach { singleInputString ->
+            val inputSplit = singleInputString.split(":")
+            val gameNumber = inputSplit[0].replace("Game ","").toInt()
+            possibleIDList.add(gameNumber)
+
+            val fullGame = inputSplit[1].trim()
+            val games = fullGame.split(";")
+            handlePowerGame(games)
+        }
+        allPowers.println()
+        return allPowers.sum()
     }
 
     // test if implementation meets criteria from the description, like:
@@ -52,6 +37,72 @@ fun main() {
     //check(part1(testInput) == 1)
 
     //val input = readInput("Day01")
-    part1(testInput).println()
+    part2(testInput).println()
     //part2(input).println()
 }
+fun handleInvalidGames(games: List<String>, gameNumber: Int){
+    val MAX_RED = 12
+    val MAX_GREEN = 13
+    val MAX_BLUE = 14
+
+    for(game in games){
+        val gameSplit = game.trim().split(",")
+        gameSplit.println()
+        gameSplit.forEach { singleCube ->
+            when {
+                singleCube.contains("red") -> {
+                    val redNum = singleCube.replace(" red","").trim()
+                    if (redNum.toInt() > MAX_RED){
+                        possibleIDList.remove(gameNumber)
+                    }
+                }
+                singleCube.contains("green") -> {
+                    val greenNum = singleCube.replace(" green","").trim()
+                    if (greenNum.toInt() > MAX_GREEN){
+                        possibleIDList.remove(gameNumber)
+                    }
+                }
+                singleCube.contains("blue") -> {
+                    val blueNum = singleCube.replace(" blue","").trim()
+                    if (blueNum.toInt() > MAX_BLUE){
+                        possibleIDList.remove(gameNumber)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun handlePowerGame(games: List<String>){
+    var MIN_RED = 0
+    var MIN_GREEN = 0
+    var MIN_BLUE = 0
+    for(game in games){
+        val gameSplit = game.trim().split(",")
+        gameSplit.println()
+        gameSplit.forEach { singleCube ->
+            when {
+                singleCube.contains("red") -> {
+                    val redNum = singleCube.replace(" red","").trim()
+                    if (redNum.toInt() > MIN_RED){
+                        MIN_RED = redNum.toInt()
+                    }
+                }
+                singleCube.contains("green") -> {
+                    val greenNum = singleCube.replace(" green","").trim()
+                    if (greenNum.toInt() > MIN_GREEN){
+                        MIN_GREEN = greenNum.toInt()
+                    }
+                }
+                singleCube.contains("blue") -> {
+                    val blueNum = singleCube.replace(" blue","").trim()
+                    if (blueNum.toInt() > MIN_BLUE){
+                        MIN_BLUE = blueNum.toInt()
+                    }
+                }
+            }
+        }
+    }
+    allPowers.add(MIN_RED*MIN_BLUE*MIN_GREEN)
+}
+
